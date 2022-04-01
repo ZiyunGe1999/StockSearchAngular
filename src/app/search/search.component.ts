3f46cbe86c0e83362b014863cf1a3036cf6f296b
Ziyun Ge <ziyunge1999@gmail.com>
2022-03-31 17:39:24 -0700

    finish search bar without http request

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable, OperatorFunction} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+// import { FormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
 @Component({
   selector: 'app-search',
@@ -7,9 +20,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  public model: any;
+  init_color = "#2323AC";
+  search_color = this.init_color;
+  times_color = this.init_color;
+
+  searchForm = this.formBuilder.group({
+    symbol: ''
+  });
+
+  search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term.length < 2 ? []
+        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+    )
+
+  cleanForm() {
+    this.searchForm.reset();
+  }
+
+  changeColor(which_one : string, color : string) {
+    if (which_one == 'times') {
+      this.times_color = color;
+    }
+    else {
+      this.search_color = color;
+    }
+  }
+
+  onSubmit(): void {
+    window.alert('symbol: ' + this.searchForm.value['symbol']);
   }
 
 }
